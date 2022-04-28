64a4b9856ecf4c8e690e84e945a74f3f4aa15b6f
steveghost33 <96503348+steveghost33@users.noreply.github.com>
2022-04-28 16:22:30 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ function RenderCampsite({campsite}) {
         );
     }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -29,7 +29,6 @@ function RenderComments({comments}) {
                     {comments.map(comments => {
                         return (
                             <div key={comments.id}>
-                                <p>{comments.text}</p>
                                 <p>
                                     {comments.text}<br />
                                     -- {comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}
@@ -37,37 +36,13 @@ function RenderComments({comments}) {
                                     </div>
                                 );
                             })}
-                            <CommentForm/>
+                            <CommentForm campsiteId={campsiteId} addComment={addComment} />
                           </div>
                         );
                       }
                       return (<div />
                       );
                     }
-
-function CampsiteInfo(props) {
-    if (props.campsite) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <h2>{props.campsite.name}</h2>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
-                </div>
-            </div>
-        );
-    }
-    return <div />;
-}
 
 class CommentForm extends Component {
     constructor (props) {
@@ -97,9 +72,9 @@ toggleModal() {
 }
 
 handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is " + JSON.stringify(values));
-};
+    this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+}
 
 render () {
     return (
@@ -154,5 +129,33 @@ render () {
          )
     }  
 } 
+
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
+                </div>
+            </div>
+        );
+    }
+    return <div />;
+}
 
 export default CampsiteInfo;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 
@@ -16,6 +17,10 @@ const mapStateToProps = state => {
         partners: state.partners,
         promotions: state.promotions
     };
+};
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
 };
 
 class Main extends Component {
@@ -34,8 +39,10 @@ class Main extends Component {
 
     const CampsiteWithId = ({match}) => {
         return (
-            <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
-              comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+            <CampsiteInfo 
+                campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+                comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+                addComment={this.props.addComment} />
         );
     };
 
@@ -56,4 +63,4 @@ class Main extends Component {
 }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
